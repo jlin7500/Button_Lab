85a1f6b0b35da4c8b2fe2e6543aba22c5d82ea36
BT_1N3_26 <jlin7500@bths.edu>
2019-01-09 16:24:13 -0500

    asd

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -9,10 +11,34 @@ public class Controller
     private int counter;
     @FXML
     private Label display;
-
+    @FXML
+    private Label display1;
+    private boolean switchs = true;
     public void add()
     {
-        counter++;
-        display.setText(String.valueOf(counter));
+        if(switchs)
+        {
+            counter++;
+            display.setText("");//(String.valueOf(counter));
+            display1.setText(String.valueOf(counter));
+        }
+    }
+    long karen = System.nanoTime() + 10000000000L;
+    @FXML
+    public void initialize()
+    {
+        new AnimationTimer()
+        {
+            public void handle(long now)
+            {
+                display.setText("Time :" + now);
+                if (now > karen)
+                {
+                    this.stop();
+                    switchs = false;
+                    display1.setText("You clicked" + " " + counter + " " + "times!");
+                }
+            }
+        }.start();
     }
 }
